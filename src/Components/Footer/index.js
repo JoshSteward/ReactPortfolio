3eaac5f1a80c69d4730e8219af072d74a0a2d270
Josh Steward <stewardjosh10@gmail.com>
2021-04-25 15:18:28 +1000

    Header and Footer added

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <span> Joshua Steward - React Portfolio</span>
+      </footer>
+    );
+  }
+  
+  export default Footer;
